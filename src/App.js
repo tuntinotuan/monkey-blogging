9c845b195d4c0c50b00b3c9ebb08bcc59394bbb7
tuntinotuan <tuntino666@gmail.com>
2023-06-19 17:38:18 +0700

    add authContext

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Routes } from "react-router-dom";
+import { AuthProvider } from "./contexts/auth-context";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <AuthProvider>
+        <Routes></Routes>
+      </AuthProvider>
+    </div>
+  );
 }
 
 export default App;
