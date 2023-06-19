43fbc7cddf418a42007373f65d88c18dec18bee2
tuntinotuan <tuntino666@gmail.com>
2023-06-20 00:48:08 +0700

    UI Basic SignUpPage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import { Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { AuthProvider } from "./contexts/auth-context";
+import SignUpPage from "./pages/SignUpPage";
 
 function App() {
   return (
     <div>
       <AuthProvider>
-        <Routes></Routes>
+        <Routes>
+          <Route path="/sign-up" element={<SignUpPage></SignUpPage>}></Route>
+        </Routes>
       </AuthProvider>
     </div>
   );
