7559c1963d92687924cc92c420664294ed25b87b
tuntinotuan <tuntino666@gmail.com>
2023-06-20 15:14:38 +0700

    optimize component Input

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,5 +1,8 @@
+import { IconEyeClose, IconEyeOpen } from "components/icon";
+import { Input } from "components/input";
 import { Label } from "components/label";
 import React from "react";
+import { useForm } from "react-hook-form";
 import styled from "styled-components";
 
 const SignUpPageStyles = styled.div`
@@ -21,30 +24,6 @@ const SignUpPageStyles = styled.div`
     align-items: flex-start;
     row-gap: 20px;
   }
-  /* .label {
-    color: ${(props) => props.theme.grayDark};
-    font-weight: 600;
-    cursor: pointer;
-  } */
-  .input {
-    width: 100%;
-    padding: 20px;
-    background-color: ${(props) => props.theme.grayLight};
-    border-radius: 8px;
-    font-weight: 500;
-    transition: all 0.2s linear;
-    border: 1px solid transparent;
-  }
-  .input:focus {
-    background-color: white;
-    border-color: ${(props) => props.theme.primary};
-  }
-  .input::-webkit-input-placeholder {
-    color: #84878b;
-  }
-  .input::-moz-input-placeholder {
-    color: #84878b;
-  }
   .form {
     max-width: 600px;
     margin: 0 auto;
@@ -52,20 +31,31 @@ const SignUpPageStyles = styled.div`
 `;
 
 const SignUpPage = () => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors, isValid, isSubmitting },
+    watch,
+  } = useForm({});
+  const handleSignUp = (values) => {
+    console.log(values);
+  };
   return (
     <SignUpPageStyles>
       <div className="container">
         <img srcSet="/logo.png 2x" alt="monkey-blogging" className="logo" />
         <h1 className="heading">Monkey Blogging</h1>
-        <form action="" className="form">
+        <form action="" className="form" onSubmit={handleSubmit(handleSignUp)}>
           <div className="field">
             <Label htmlFor="fullname">Fullname</Label>
-            <input
-              id="fullname"
+            <Input
               type="text"
-              className="input"
+              name="fullname"
               placeholder="Enter your fullname"
-            />
+              control={control}
+            >
+              <IconEyeClose className="input-icon"></IconEyeClose>
+            </Input>
           </div>
         </form>
       </div>
