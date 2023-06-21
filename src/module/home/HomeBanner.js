6820cdbe602a9a654d9bb71f82fa1bb0652d5ae9
tuntinotuan <tuntino666@gmail.com>
2023-06-21 17:08:49 +0700

    UI HomeBanner

---
+++ src/module/home/HomeBanner.js
@@ -0,0 +1,57 @@
+import { Button } from "components/button";
+import React from "react";
+import styled from "styled-components";
+
+const HomeBannerStyles = styled.div`
+  min-height: 520px;
+  padding: 40px 0;
+  background-image: linear-gradient(
+    to right bottom,
+    ${(props) => props.theme.primary},
+    ${(props) => props.theme.secondary}
+  );
+  .banner {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    &-content {
+      max-width: 600px;
+      color: white;
+    }
+    &-heading {
+      font-size: 36px;
+      margin-bottom: 20px;
+    }
+    &-desc {
+      line-height: 1.75;
+      margin-bottom: 40px;
+    }
+  }
+`;
+
+const HomeBanner = () => {
+  return (
+    <HomeBannerStyles>
+      <div className="container">
+        <div className="banner">
+          <div className="banner-content">
+            <h1 className="banner-heading">Monkey Blogging</h1>
+            <p className="banner-desc">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum
+              libero, modi sit voluptatem harum ullam ipsum sint, dicta non, ut
+              quisquam et quod delectus rem nemo fugit voluptates impedit est.
+            </p>
+            <Button to="/sign-in" kind="secondary">
+              Get Started
+            </Button>
+          </div>
+          <div className="banner-image">
+            <img srcSet="/img-banner.png 2x" alt="banner" />
+          </div>
+        </div>
+      </div>
+    </HomeBannerStyles>
+  );
+};
+
+export default HomeBanner;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,19 +1,16 @@
-import Header from "components/layout/Header";
-import { auth } from "firebase-app/firebase-config";
-import { signOut } from "firebase/auth";
+import Layout from "components/layout/Layout";
+import HomeBanner from "module/home/HomeBanner";
 import React from "react";
 import styled from "styled-components";
 
 const HomePageStyles = styled.div``;
 
 const HomePage = () => {
-  const handleSignOut = () => {
-    signOut(auth);
-    console.log("Sign out successfully!!!");
-  };
   return (
     <HomePageStyles>
-      <Header></Header>
+      <Layout>
+        <HomeBanner></HomeBanner>
+      </Layout>
     </HomePageStyles>
   );
 };
