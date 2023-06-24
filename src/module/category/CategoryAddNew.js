9173952e641e8e42740a7fc77654a4ade0e31c72
tuntinotuan <tuntino666@gmail.com>
2023-06-24 15:42:32 +0700

    UI CategoryAddNew

---
+++ src/module/category/CategoryAddNew.js
@@ -0,0 +1,74 @@
+import { Button } from "components/button";
+import { Radio } from "components/checkbox";
+import { Field, FieldCheckboxes } from "components/field";
+import { Input } from "components/input";
+import { Label } from "components/label";
+import DashboardHeading from "module/dashboard/DashboardHeading";
+import React from "react";
+import { useForm } from "react-hook-form";
+
+const CategoryAddNew = () => {
+  const {
+    control,
+    setValue,
+    handleSubmit,
+    formState: { errors, isSubmitting, isValid },
+  } = useForm({
+    mode: "onChange",
+    defaultValues: {
+      title: "",
+      slug: "",
+      status: 1,
+      createdAt: new Date(),
+    },
+  });
+  const handleAddNewCategory = (values) => {
+    console.log(values);
+  };
+  return (
+    <div>
+      <DashboardHeading
+        title="New category"
+        desc="Add new category"
+      ></DashboardHeading>
+      <form onSubmit={handleSubmit(handleAddNewCategory)}>
+        <div className="form-layout">
+          <Field>
+            <Label>Name</Label>
+            <Input
+              control={control}
+              name="name"
+              placeholder="Enter your category name"
+            ></Input>
+          </Field>
+          <Field>
+            <Label>Slug</Label>
+            <Input
+              control={control}
+              name="slug"
+              placeholder="Enter your slug"
+            ></Input>
+          </Field>
+        </div>
+        <div className="form-layout">
+          <Field>
+            <Label>Status</Label>
+            <FieldCheckboxes>
+              <Radio name="status" control={control} checked={true}>
+                Approved
+              </Radio>
+              <Radio name="status" control={control}>
+                Unapproved
+              </Radio>
+            </FieldCheckboxes>
+          </Field>
+        </div>
+        <Button type="submit" className="mx-auto">
+          Add new category
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default CategoryAddNew;
