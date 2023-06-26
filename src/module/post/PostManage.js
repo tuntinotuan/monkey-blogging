680fb7b83bd3764b00bd81ae794ff4cc6ebe16c2
tuntinotuan <tuntino666@gmail.com>
2023-06-27 02:19:14 +0700

    call data for PostManage component

--- src/module/post/PostManage.js
+++ src/module/post/PostManage.js
@@ -1,9 +1,58 @@
+import { ActionDelete, ActionEdit, ActionView } from "components/action";
+import { Button } from "components/button";
 import { Pagination } from "components/pagination";
 import { Table } from "components/table";
+import { db } from "firebase-app/firebase-config";
+import {
+  collection,
+  getDocs,
+  limit,
+  onSnapshot,
+  query,
+  where,
+} from "firebase/firestore";
+import { debounce } from "lodash";
 import DashboardHeading from "module/dashboard/DashboardHeading";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const POST_PER_PAGE = 4;
 
 const PostManage = () => {
+  const navigate = useNavigate();
+  const [postList, setPostList] = useState([]);
+  const [filter, setFilter] = useState("");
+  const [currentDocument, setCurrentdocument] = useState("");
+  useEffect(() => {
+    async function fetchData() {
+      const colRef = collection(db, "posts");
+      const newRef = filter
+        ? query(
+            colRef,
+            where("title", ">=", filter),
+            where("title", "<=", filter + "utf8")
+          )
+        : query(colRef, limit(POST_PER_PAGE));
+      const documentSnapshots = await getDocs(newRef);
+      const lastVisible =
+        documentSnapshots.docs[documentSnapshots.docs.length - 1];
+      onSnapshot(newRef, (snapshot) => {
+        let results = [];
+        snapshot.forEach((doc) => {
+          results.push({
+            id: doc.id,
+            ...doc.data(),
+          });
+        });
+        setPostList(results);
+      });
+      setCurrentdocument(lastVisible);
+    }
+    fetchData();
+  }, [filter]);
+  const handleSearchPost = debounce((e) => {
+    setFilter(e.target.value);
+  }, 500);
   return (
     <div>
       <DashboardHeading
@@ -16,13 +65,13 @@ const PostManage = () => {
             type="text"
             className="w-full p-4 rounded-lg border border-solid border-gray-300"
             placeholder="Search post..."
+            onChange={handleSearchPost}
           />
         </div>
       </div>
       <Table>
         <thead>
           <tr>
-            <th></th>
             <th>Id</th>
             <th>Post</th>
             <th>Category</th>
@@ -31,92 +80,54 @@ const PostManage = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td></td>
-            <td>01</td>
-            <td>
-              <div className="flex items-center gap-x-3">
-                <img
-                  src="https://images.unsplash.com/photo-1620641788421-7a1c342ea42e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1548&q=80"
-                  alt=""
-                  className="w-[66px] h-[55px] rounded object-cover"
-                />
-                <div className="flex-1">
-                  <h3 className="font-semibold">One Special 4K Camera</h3>
-                  <time className="text-sm text-gray-500">
-                    Date: 25 Oct 2021
-                  </time>
-                </div>
-              </div>
-            </td>
-            <td>
-              <span className="text-gray-500">Camera Gear</span>
-            </td>
-            <td>
-              <span className="text-gray-500">Evondev</span>
-            </td>
-            <td>
-              <div className="flex items-center gap-x-3 text-gray-500">
-                <span className="flex items-center justify-center w-10 h-10 border border-gray-200 rounded cursor-pointer">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-5 h-5"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+          {postList.length > 0 &&
+            postList.map((post) => (
+              <tr>
+                <td>{post.id.slice(0, 5) + "..."}</td>
+                <td>
+                  <div className="flex items-center gap-x-3">
+                    <img
+                      src={post.image}
+                      alt=""
+                      className="w-[66px] h-[55px] rounded object-cover"
                     />
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
-                    />
-                  </svg>
-                </span>
-                <span className="flex items-center justify-center w-10 h-10 border border-gray-200 rounded cursor-pointer">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-5 h-5"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
-                    />
-                  </svg>
-                </span>
-                <span className="flex items-center justify-center w-10 h-10 border border-gray-200 rounded cursor-pointer">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-5 h-5"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-                    />
-                  </svg>
-                </span>
-              </div>
-            </td>
-          </tr>
+                    <div className="flex-1">
+                      <h3 className="font-semibold max-w-[200px] whitespace-pre-wrap">
+                        {post.title}
+                      </h3>
+                      <time className="text-sm text-gray-500">
+                        Date:
+                        {new Date(
+                          post?.createdAt?.seconds * 1000
+                        ).toLocaleDateString("vi-VI")}
+                      </time>
+                    </div>
+                  </div>
+                </td>
+                <td>
+                  <span className="text-gray-500">{post?.category.name}</span>
+                </td>
+                <td>
+                  <span className="text-gray-500">{post?.user?.fullname}</span>
+                </td>
+                <td>
+                  <div className="flex items-center gap-x-3 text-gray-500">
+                    <ActionView
+                      onClick={() => navigate(`/${post.slug}`)}
+                    ></ActionView>
+                    <ActionEdit></ActionEdit>
+                    <ActionDelete></ActionDelete>
+                  </div>
+                </td>
+              </tr>
+            ))}
         </tbody>
       </Table>
       <div className="mt-10">
-        <Pagination></Pagination>
+        {/* <Pagination></Pagination> */}
+        <Button className="mx-auto w-[200px]" onClick={() => {}}>
+          Load more
+        </Button>
       </div>
     </div>
   );
