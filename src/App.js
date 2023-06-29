c919476e6a61c386d38dbb4706da9602694c8cf7
tuntinotuan <tuntino666@gmail.com>
2023-06-29 23:45:28 +0700

    optimize App with Suspense lazy loading

--- src/App.js
+++ src/App.js
@@ -1,88 +1,109 @@
+import React, { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import { AuthProvider } from "./contexts/auth-context";
-import SignUpPage from "./pages/SignUpPage";
-import SignInPage from "pages/SignInPage";
-import HomePage from "pages/HomePage";
-import PageNotFound from "pages/PageNotFound";
-import PostDetailsPage from "pages/PostDetailsPage";
-import DashboardLayout from "module/dashboard/DashboardLayout";
-import DashboardPage from "pages/DashboardPage";
-import PostManage from "module/post/PostManage";
-import PostAddNew from "module/post/PostAddNew";
-import CategoryAddNew from "module/category/CategoryAddNew";
-import UserAddNew from "module/user/UserAddNew";
-import UserProfile from "module/user/UserProfile";
-import CategoryManage from "module/category/CategoryManage";
-import CategoryUpdate from "module/category/CategoryUpdate";
-import UserManage from "module/user/UserManage";
-import UserUpdate from "module/user/UserUpdate";
-import PostUpdate from "module/post/PostUpdate";
-import CagetoryPage from "pages/CagetoryPage";
+import { LoadingSpinner } from "components/loading";
+const HomePage = React.lazy(() => import("pages/HomePage"));
+const SignInPage = React.lazy(() => import("pages/SignInPage"));
+const SignUpPage = React.lazy(() => import("pages/SignUpPage"));
+const PageNotFound = React.lazy(() => import("pages/PageNotFound"));
+const CagetoryPage = React.lazy(() => import("pages/CagetoryPage"));
+const PostDetailsPage = React.lazy(() => import("pages/PostDetailsPage"));
+const PostUpdate = React.lazy(() => import("module/post/PostUpdate"));
+const UserUpdate = React.lazy(() => import("module/user/UserUpdate"));
+const UserManage = React.lazy(() => import("module/user/UserManage"));
+const UserProfile = React.lazy(() => import("module/user/UserProfile"));
+const UserAddNew = React.lazy(() => import("module/user/UserAddNew"));
+const PostAddNew = React.lazy(() => import("module/post/PostAddNew"));
+const PostManage = React.lazy(() => import("module/post/PostManage"));
+const DashboardPage = React.lazy(() => import("pages/DashboardPage"));
+const DashboardLayout = React.lazy(() =>
+  import("module/dashboard/DashboardLayout")
+);
+const CategoryUpdate = React.lazy(() =>
+  import("module/category/CategoryUpdate")
+);
+const CategoryManage = React.lazy(() =>
+  import("module/category/CategoryManage")
+);
+const CategoryAddNew = React.lazy(() =>
+  import("module/category/CategoryAddNew")
+);
 
 function App() {
   return (
     <div>
       <AuthProvider>
-        <Routes>
-          <Route path="/" element={<HomePage></HomePage>}></Route>
-          <Route
-            path="/category/:slug"
-            element={<CagetoryPage></CagetoryPage>}
-          ></Route>
-          <Route path="/sign-up" element={<SignUpPage></SignUpPage>}></Route>
-          <Route path="/sign-in" element={<SignInPage></SignInPage>}></Route>
-          <Route path="*" element={<PageNotFound></PageNotFound>}></Route>
-          <Route
-            path="/:slug"
-            element={<PostDetailsPage></PostDetailsPage>}
-          ></Route>
-          <Route element={<DashboardLayout></DashboardLayout>}>
+        <Suspense
+          fallback={
+            <div className="h-[100vh] mx-auto flex items-center justify-center">
+              <LoadingSpinner
+                size="120px"
+                borderColor="#2EBAC1"
+              ></LoadingSpinner>
+            </div>
+          }
+        >
+          <Routes>
+            <Route path="/" element={<HomePage></HomePage>}></Route>
             <Route
-              path="/dashboard"
-              element={<DashboardPage></DashboardPage>}
+              path="/category/:slug"
+              element={<CagetoryPage></CagetoryPage>}
             ></Route>
+            <Route path="/sign-up" element={<SignUpPage></SignUpPage>}></Route>
+            <Route path="/sign-in" element={<SignInPage></SignInPage>}></Route>
+            <Route path="*" element={<PageNotFound></PageNotFound>}></Route>
             <Route
-              path="/manage/post"
-              element={<PostManage></PostManage>}
+              path="/:slug"
+              element={<PostDetailsPage></PostDetailsPage>}
             ></Route>
-            <Route
-              path="/manage/add-post"
-              element={<PostAddNew></PostAddNew>}
-            ></Route>
-            <Route
-              path="/manage/update-post"
-              element={<PostUpdate></PostUpdate>}
-            ></Route>
-            <Route
-              path="/manage/category"
-              element={<CategoryManage></CategoryManage>}
-            ></Route>
-            <Route
-              path="/manage/add-category"
-              element={<CategoryAddNew></CategoryAddNew>}
-            ></Route>
-            <Route
-              path="/manage/update-category"
-              element={<CategoryUpdate></CategoryUpdate>}
-            ></Route>
-            <Route
-              path="/manage/user"
-              element={<UserManage></UserManage>}
-            ></Route>
-            <Route
-              path="/manage/add-user"
-              element={<UserAddNew></UserAddNew>}
-            ></Route>
-            <Route
-              path="/manage/update-user"
-              element={<UserUpdate></UserUpdate>}
-            ></Route>
-            <Route
-              path="/profile"
-              element={<UserProfile></UserProfile>}
-            ></Route>
-          </Route>
-        </Routes>
+            <Route element={<DashboardLayout></DashboardLayout>}>
+              <Route
+                path="/dashboard"
+                element={<DashboardPage></DashboardPage>}
+              ></Route>
+              <Route
+                path="/manage/post"
+                element={<PostManage></PostManage>}
+              ></Route>
+              <Route
+                path="/manage/add-post"
+                element={<PostAddNew></PostAddNew>}
+              ></Route>
+              <Route
+                path="/manage/update-post"
+                element={<PostUpdate></PostUpdate>}
+              ></Route>
+              <Route
+                path="/manage/category"
+                element={<CategoryManage></CategoryManage>}
+              ></Route>
+              <Route
+                path="/manage/add-category"
+                element={<CategoryAddNew></CategoryAddNew>}
+              ></Route>
+              <Route
+                path="/manage/update-category"
+                element={<CategoryUpdate></CategoryUpdate>}
+              ></Route>
+              <Route
+                path="/manage/user"
+                element={<UserManage></UserManage>}
+              ></Route>
+              <Route
+                path="/manage/add-user"
+                element={<UserAddNew></UserAddNew>}
+              ></Route>
+              <Route
+                path="/manage/update-user"
+                element={<UserUpdate></UserUpdate>}
+              ></Route>
+              <Route
+                path="/profile"
+                element={<UserProfile></UserProfile>}
+              ></Route>
+            </Route>
+          </Routes>
+        </Suspense>
       </AuthProvider>
     </div>
   );
--- src/components/loading/LoadingSpinner.js
+++ src/components/loading/LoadingSpinner.js
@@ -2,9 +2,10 @@ import React from "react";
 import styled from "styled-components";
 
 const SpinnerStyles = styled.div`
+  z-index: 1000;
   width: ${(props) => props.size};
   height: ${(props) => props.size};
-  border: ${(props) => props.borderSize} solid white;
+  border: ${(props) => props.borderSize} solid ${(props) => props.borderColor};
   border-top: ${(props) => props.borderSize} solid transparent;
   border-bottom: ${(props) => props.borderSize} solid transparent;
   border-radius: 100rem;
@@ -17,8 +18,18 @@ const SpinnerStyles = styled.div`
   }
 `;
 
-const LoadingSpinner = ({ size = "40px", borderSize = "5px" }) => {
-  return <SpinnerStyles size={size} borderSize={borderSize}></SpinnerStyles>;
+const LoadingSpinner = ({
+  size = "40px",
+  borderSize = "5px",
+  borderColor = "white",
+}) => {
+  return (
+    <SpinnerStyles
+      size={size}
+      borderSize={borderSize}
+      borderColor={borderColor}
+    ></SpinnerStyles>
+  );
 };
 
 export default LoadingSpinner;
