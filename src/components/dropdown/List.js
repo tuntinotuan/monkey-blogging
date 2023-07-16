5bcf99638ccd92dfc9c94f14991778bd928e91ae
tuntinotuan <tuntino666@gmail.com>
2023-07-17 00:16:26 +0700

    Update Dropdown with createPortal

--- src/components/dropdown/List.js
+++ src/components/dropdown/List.js
@@ -1,16 +1,26 @@
 import React from "react";
 import { useDropdown } from "./dropdown-context";
+import ReactDOM from "react-dom";
 
 const List = ({ children }) => {
-  const { show } = useDropdown();
-  return (
-    <>
-      {show && (
-        <div className="absolute top-full left-0 w-full bg-white shadow-md">
-          {children}
-        </div>
-      )}
-    </>
+  const { show, coords } = useDropdown();
+  if (typeof document === "undefined") return <></>;
+  return ReactDOM.createPortal(
+    <div
+      className={`absolute top-0left-0w-full rounded bg-white shadow-md transition-all duration-300 ${
+        show ? "opacity-100 visible" : "opacity-0 invisible"
+      }`}
+      style={
+        coords && {
+          top: coords.top + coords.height + window.scrollY + 1,
+          left: coords.left,
+          width: coords.width,
+        }
+      }
+    >
+      {children}
+    </div>,
+    document.querySelector("body")
   );
 };
 
--- src/components/dropdown/dropdown-context.js
+++ src/components/dropdown/dropdown-context.js
@@ -1,12 +1,32 @@
+import { useEffect } from "react";
+import { useRef } from "react";
 import { createContext, useContext, useState } from "react";
 
 const DropdownContext = createContext();
 function DropdownProvider(props) {
+  const nodeRef = useRef(null);
   const [show, setShow] = useState(false);
-  const toggle = () => {
+  const [coords, setCoords] = useState(null);
+  const toggle = (e) => {
     setShow(!show);
+    setCoords(e.currentTarget.getBoundingClientRect());
   };
-  const values = { show, setShow, toggle };
+  useEffect(() => {
+    function handleClickOutSide(e) {
+      if (
+        nodeRef.current &&
+        !nodeRef.current.contains(e.target) &&
+        !e.target.matches("button")
+      ) {
+        setShow(false);
+      }
+    }
+    document.addEventListener("click", handleClickOutSide);
+    return () => {
+      document.removeEventListener("click", handleClickOutSide);
+    };
+  }, []);
+  const values = { show, setShow, toggle, coords, setCoords, nodeRef };
   return (
     <DropdownContext.Provider value={values}>
       {props.children}
